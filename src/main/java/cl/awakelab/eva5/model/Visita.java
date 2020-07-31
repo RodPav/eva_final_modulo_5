35f5187bf5c62c4c72a4eb883c4e86563819465c
Sebastian <sebaram@gmail.com>
2020-07-30 23:19:19 -0400

    Se modifica la clase Visita, actualizada con notaciones JPA

--- src/main/java/cl/awakelab/eva5/model/Visita.java
+++ src/main/java/cl/awakelab/eva5/model/Visita.java
@@ -2,75 +2,94 @@ package cl.awakelab.eva5.model;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="visita")
 
 public class Visita {
-
-	private int idVisita;
-	private LocalDate fechaVisita;
-	private LocalTime horaVisita;
-	private String dirVisita;
-	private ArrayList<Visita> listaChequeo;
 	
-	public Visita( LocalDate fechaVisita, LocalTime horaVisita, String dirVisita,
-			ArrayList<Visita> listaChequeo) {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id_visita")
+	private int idVisita;
+	
+	@Column(name="fecha_visita")
+	private LocalDate fechaVisita;
+	
+	@Column(name="hora_visita")
+	private LocalTime horaVisita;
+	
+	
+	
+	@JoinColumn(name="cliente_id_cliente")
+	@OneToOne
+	private Cliente cliente;
+	
+	@JoinColumn(name="profesional_id_profesional")
+	@OneToOne
+	private Profesional profesional;
+	
+	
+	
+	public Visita(int idVisita, LocalDate fechaVisita, LocalTime horaVisita, Cliente cliente, Profesional profesional) {
 		super();
-
+		this.idVisita = idVisita;
 		this.fechaVisita = fechaVisita;
 		this.horaVisita = horaVisita;
-		this.dirVisita = dirVisita;
-		this.listaChequeo = listaChequeo;
+		this.cliente = cliente;
+		this.profesional = profesional;
 	}
-
+	
+	
 	public int getIdVisita() {
 		return idVisita;
 	}
-
 	public void setIdVisita(int idVisita) {
 		this.idVisita = idVisita;
 	}
-
 	public LocalDate getFechaVisita() {
 		return fechaVisita;
 	}
-
 	public void setFechaVisita(LocalDate fechaVisita) {
 		this.fechaVisita = fechaVisita;
 	}
-
 	public LocalTime getHoraVisita() {
 		return horaVisita;
 	}
-
 	public void setHoraVisita(LocalTime horaVisita) {
 		this.horaVisita = horaVisita;
 	}
-
-	public String getDirVisita() {
-		return dirVisita;
+	public Cliente getCliente() {
+		return cliente;
 	}
-
-	public void setDirVisita(String dirVisita) {
-		this.dirVisita = dirVisita;
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
 	}
-
-	public ArrayList<Visita> getListaChequeo() {
-		return listaChequeo;
+	public Profesional getProfesional() {
+		return profesional;
 	}
-
-	public void setListaChequeo(ArrayList<Visita> listaChequeo) {
-		this.listaChequeo = listaChequeo;
+	public void setProfesional(Profesional profesional) {
+		this.profesional = profesional;
 	}
-
+	
+	
 	@Override
 	public String toString() {
 		return "Visita [idVisita=" + idVisita + ", fechaVisita=" + fechaVisita + ", horaVisita=" + horaVisita
-				+ ", dirVisita=" + dirVisita + ", listaChequeo=" + listaChequeo + "]";
+				+ ", cliente=" + cliente + ", profesional=" + profesional + "]";
 	}
 	
-	
 
-	
-	
+
 	
 }
